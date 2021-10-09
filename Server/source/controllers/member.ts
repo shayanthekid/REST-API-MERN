bec00e77dc1eee61d1687d4a78a26c8630350bc0
shayanthekid <sajidfayazhaniff@gmail.com>
2021-10-09 23:14:10 +0530

    Member CRUD fully completed

--- Server/source/controllers/member.ts
+++ Server/source/controllers/member.ts
@@ -6,7 +6,6 @@ const NAMESPACE = 'Members';
 
 //Create new Member
 const register = (req: Request, res: Response, next: NextFunction) => {
-
     let { Name, Email, Address, Birthdate, Entrancedate } = req.body;
 
     const _member = new Member({
@@ -18,24 +17,111 @@ const register = (req: Request, res: Response, next: NextFunction) => {
         Entrancedate
     });
     return _member
-    .save()
-    .then((member)=>{
-        return res.status(201).json({
-            member
+        .save()
+        .then((member) => {
+            return res.status(201).json({
+                member
+            });
+        })
+        .catch((error) => {
+            return res.status(500).json({
+                message: error.message,
+                error
+            });
         });
-    }).catch((error) => {
+};
+
+const getAllMembers = (req: Request, res: Response, next: NextFunction) => {
+    Member.find()
+        .exec()
+        .then((members) => {
+            return res.status(200).json({
+                members,
+                count: members.length
+            });
+        })
+        .catch((error) => {
+            return res.status(500).json({
+                message: error.message,
+                error
+            });
+        });
+};
+
+const getOneMember = (req: Request, res: Response, next: NextFunction) => {
+    let { _id } = req.body;
+
+    Member.findById(_id)
+        .exec()
+        .then((members) => {
+            return res.status(200).json({
+                members
+            });
+        })
+        .catch((error) => {
+            return res.status(500).json({
+                message: error.message,
+                error
+            });
+        });
+};
+
+const updateMember = (req: Request, res: Response, next: NextFunction) => {
+    let { Name, Email, Address, Birthdate, Entrancedate, _id } = req.body;
+
+    const filter = {
+        _id
+    };
+    const New_member = {
+        Name,
+        Email,
+        Address,
+        Birthdate,
+        Entrancedate
+    };
+
+    Member.updateOne(filter, New_member, {
+        new: true
+    })
+        .exec()
+        .then((members) => {
+            return res.status(200).json({
+                message: 'Successfully updated',
+                members
+            });
+        })
+        .catch((error) => {
+            return res.status(500).json({
+                message: error.message,
+                error
+            });
+        });
+};
+
+const deleteMember = (req: Request, res: Response, next: NextFunction) => {
+    let { _id } = req.body;
+    const filter = {
+        _id
+    };
+
+        Member.deleteOne(filter)
+            .then(() => {
+                return res.status(200).json({
+                    message: 'Successfully Deleted Member'
+                });
+            })
+            .catch((error) => {
                 return res.status(500).json({
                     message: error.message,
                     error
                 });
+            });
 
-})
-}
-
-const getAllMembers = (req: Request, res: Response, next: NextFunction) => {
-
-
-}
+};
 export default {
-    register
+    register,
+    getAllMembers,
+    getOneMember,
+    updateMember,
+    deleteMember
 };
--- Server/source/routes/member.ts
+++ Server/source/routes/member.ts
@@ -4,5 +4,9 @@ import controller from '../controllers/member';
 const router = express.Router();
 
 router.post('/register',  controller.register);
+router.get('/getAllMembers', controller.getAllMembers);
+router.get('/getOneMember', controller.getOneMember);
+router.put('/updateMember', controller.updateMember);
+router.delete('/deleteMember', controller.deleteMember);
 
 export = router;
