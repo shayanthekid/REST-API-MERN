07b3056316ca27decdda42d629124526c6ed2d78
shayanthekid <sajidfayazhaniff@gmail.com>
2021-10-11 22:45:53 +0530

    Displaying members completed. CUD + validations need to be added

--- Client/member-front/src/pages/Home.tsx
+++ Client/member-front/src/pages/Home.tsx
@@ -1,23 +1,67 @@
-import React from 'react'
-import { useState,useEffect } from "react";
+import React, { SyntheticEvent } from "react";
+import { useState, useEffect } from "react";
 import { Redirect } from "react-router";
 function Home() {
+  interface IState {
+    Members: {
+      Name?: string;
+      Email?: string;
+      Address?: string;
+      Birthdate?: Date;
+      Entrancedate?: Date;
+    }[];
+  }
 
-    const [token, setToken] = useState("");
-   
-    useEffect(()=>{
+  const [token, setToken] = useState("");
+  const [members, setMembers] = useState<IState["Members"]>([]);
+
+ 
+
+  useEffect(() => {
     setToken(JSON.parse(localStorage.getItem("token") || "{}"));
+  });
 
-    })
+  useEffect(()=>{
+      async function displayMembers() {
+        
+       const response = await fetch(
+         "http://localhost:1337/members/getAllMembers",
+         {
+           method: "GET",
+           headers: {
+             "Content-Type": "application/json",
+           },
+         }
+       );
+       const content = await response.json();
+      
+         console.log(content.members);
+         
+       setMembers(content.members);
+    
+       
+     };
+  
+  })
+
 
   
-  if(token===""){
-      return <div>Not logged in</div>
+  if (token === "") {
+    return <div>Not logged in</div>;
+  } else {
+
+    return <div>
+
+
+      Home
+{members.map((member)=>{
+return <p>{member.Name}</p>
+})}      
+      
+      
+      
+      </div>;
   }
-  else{
-    return <div>Home</div>;
-  }
-  
 }
 
-export default Home
+export default Home;
