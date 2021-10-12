90ef5ee0279cbf1a234217c3c5d2ff5e950541c9
shayanthekid <sajidfayazhaniff@gmail.com>
2021-10-12 16:42:42 +0530

    Delete function works

--- Client/member-front/src/pages/Edit.tsx
+++ Client/member-front/src/pages/Edit.tsx
@@ -3,7 +3,7 @@ import { useState, useEffect } from "react";
 function Edit() {
   interface IState {
     Members: {
-      _id?:number;  
+      _id?: number;
       Name?: string;
       Email?: string;
       Address?: string;
@@ -38,6 +38,21 @@ function Edit() {
     // console.log(content.members);
   }, []);
   console.log(members);
+  const deleteMember = (_id: any) => {
+    fetch("http://localhost:1337/members/deleteMember", {
+      method: "DELETE",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        _id,
+      }),
+    }).then((res) => {
+      res.json().then((resp) => {
+        console.warn(resp);
+      });
+    });
+  };
 
   return (
     <div className="table table-dark">
@@ -45,25 +60,34 @@ function Edit() {
       <table>
         <thead>
           <tr>
-            <th >ID</th>
+            <th>ID</th>
             <th>Name</th>
             <th>Email</th>
             <th>Address</th>
             <th>Birthdate</th>
             <th>Entrancedate</th>
+            <th>Operations</th>
           </tr>
         </thead>
         <tbody>
-        {members.map((member) => (
-          <tr>
-            <td>{member._id}</td>
-            <td>{member.Name}</td>
-            <td>{member.Email}</td>
-            <td>{member.Address}</td>
-            <td>{member.Birthdate}</td>
-            <td>{member.Entrancedate}</td>
-          </tr>
-        ))}
+          {members.map((member) => (
+            <tr>
+              <td>{member._id}</td>
+              <td>{member.Name}</td>
+              <td>{member.Email}</td>
+              <td>{member.Address}</td>
+              <td>{member.Birthdate}</td>
+              <td>{member.Entrancedate}</td>
+              <td>
+                <button
+                  className="w-100 btn btn-lg btn-danger"
+                  onClick={() => deleteMember(member._id)}
+                >
+                  Delete
+                </button>
+              </td>
+            </tr>
+          ))}
         </tbody>
       </table>
     </div>
